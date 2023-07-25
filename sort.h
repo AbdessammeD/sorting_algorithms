66aff47b7304cb4bf9046575ba83a665ab33a385
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-07-25 15:40:42 -0700

    sorint algos

--- sort.h
+++ sort.h
@@ -1,5 +1,6 @@
 #ifndef SORT_H
-#define
+#define SORT_H
+
 #include <stdio.h>
 #include <stdlib.h>
 /**
@@ -23,5 +24,7 @@ void print_list(const listint_t *list)
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif
