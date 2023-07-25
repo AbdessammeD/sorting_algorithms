4567be83bd588e2c2cd650c7b736242287854d12
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-07-25 16:38:45 -0700

    list

--- sort.h
+++ sort.h
@@ -17,14 +17,23 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+void swapNodes(listint_t **list, listint_t *node1, listint_t *node2);
+void swap(int *a, int *b);
+int partition(int *array, int low, int high, size_t size);
+void sort(int *array, int low, int high, size_t size);
+void merge_sorted_arrays(int *array, int *temp, int l, int m, int r);
+void merge_sort_recursion(int *array, int *temp, int l, int r);
+
 
 void print_array(const int *array, size_t size);
-void print_list(const listint_t *list)
+void print_list(const listint_t *list);
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-void merge_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+
 
 #endif
